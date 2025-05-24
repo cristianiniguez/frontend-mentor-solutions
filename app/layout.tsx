4473240e8d16d3c90f83d8ec80e3444a6e0422b1
cristianiniguez <felizpc1@gmail.com>
2025-05-23 23:23:45 -0400

    nft preview card component solution fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { FC, PropsWithChildren } from "react";
 import type { Metadata } from "next";
+import './globals.css'
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- app/nft-preview-card-component/page.tsx
+++ app/nft-preview-card-component/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Outfit } from "next/font/google";
 import styles from './styles.module.scss'
 import clsx from "clsx";
@@ -7,14 +8,18 @@ const outfitSans = Outfit({
   subsets: ["latin"],
 });
 
+const IMAGES_PREFIX = '/images/nft-preview-card-component'
+
 const NftPreviewCardComponent = () => {
   return (
     <body className={clsx(outfitSans.variable, styles.body)}>
-      <div className="card">
-        <img
+      <div className={styles["card"]}>
+        <Image
           className={styles["card-image"]}
-          src="/images/image-equilibrium.jpg"
+          src={`${IMAGES_PREFIX}/image-equilibrium.jpg`}
           alt="Image Equilibrium"
+          width={300}
+          height={300}
         />
         <h1 className={styles["card-title"]}>Equilibrium #3429</h1>
         <p className={styles["card-description"]}>
@@ -23,18 +28,18 @@ const NftPreviewCardComponent = () => {
 
         <div className={styles["card-values"]}>
           <div className={clsx(styles["card-value"], styles["card-eth"])}>
-            <img src="/images/icon-ethereum.svg" alt="" />
+            <Image src={`${IMAGES_PREFIX}/icon-ethereum.svg`} alt="" width={11} height={18} />
             0.041 ETH
           </div>
           <div className={clsx(styles["card-value"], styles["card-days"])}>
-            <img src="/images/icon-clock.svg" alt="" />3 days left
+            <Image src={`${IMAGES_PREFIX}/icon-clock.svg`} alt="" width={17} height={17} />3 days left
           </div>
         </div>
 
         <hr className={styles["card-hr"]} />
 
         <div className={styles["card-avatar"]}>
-          <img src="/images/image-avatar.png" alt="" />
+          <Image src={`${IMAGES_PREFIX}/image-avatar.png`} alt="" width={38} height={38} />
           <span>
             {" "}
             Creation of <span className={styles["card-avatar-name"]}>
